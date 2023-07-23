3e57225a48d0c6844174400ae6d7d420a65abd90
lluanps <lluanps@gmail.com>
2023-07-23 20:32:24 -0300

    Projeto iniciado

---
+++ src/main/java/com/luan/invoicetracker/InvoiceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.luan.invoicetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luan/invoicetracker/InvoiceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luan.invoicetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
