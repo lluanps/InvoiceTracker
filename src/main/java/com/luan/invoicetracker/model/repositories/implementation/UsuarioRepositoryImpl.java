a6dab44bf68744240a3fe5d53147bafa2ff43d9f
lluanps <lluanps@gmail.com>
2023-07-25 02:49:59 -0300

    salvar novo usuário no metodo create

--- src/main/java/com/luan/invoicetracker/model/repositories/implementation/UsuarioRepositoryImpl.java
+++ src/main/java/com/luan/invoicetracker/model/repositories/implementation/UsuarioRepositoryImpl.java
@@ -2,13 +2,23 @@ package com.luan.invoicetracker.model.repositories.implementation;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Objects;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import com.luan.invoicetracker.enums.RoleTipo;
 import com.luan.invoicetracker.exception.ApiException;
+import com.luan.invoicetracker.model.Role;
 import com.luan.invoicetracker.model.Usuario;
+import com.luan.invoicetracker.model.repositories.RoleRepository;
 import com.luan.invoicetracker.model.repositories.UsuarioRepository;
+import com.luan.invoicetracker.query.UsuarioQuery;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,16 +27,34 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class UsuarioRepositoryImpl implements UsuarioRepository<Usuario> {
+
+	private final NamedParameterJdbcTemplate jdbc;
 	
-	private static final String COUNT_USER_EMAIL_QUERY = "";
-	private final NamedParameterJdbcTemplate jdbc; 
+	private final RoleRepository<Role> roleRepository;
+	
+	@Autowired
+	private UsuarioQuery query;
+	
+	@Autowired
+	private RoleTipo roleTipo;
 
 	@Override
 	public Usuario create(Usuario usuario) {
 		//verificar se o email é unico
 		if (getEmailCount(usuario.getEmail().trim().toLowerCase()) > 0) throw new ApiException("Email já está em uso, use outro email e tente novamente");
 		// salvar novo usuário
-		// add Role para Usuario
+		try {
+			KeyHolder holder = new GeneratedKeyHolder();
+			SqlParameterSource parameter = getSqlParameterSource(usuario);
+			jdbc.update(UsuarioQuery.INSERT_USUARIO_QUERY, parameter, holder);
+			usuario.setId(Objects.requireNonNull(holder.getKey().longValue()));
+			// add Role para Usuario
+			roleRepository.addRoleToUsuario(usuario.getId(), roleTipo.ROLE_USUARIO.name());
+		} catch (EmptyResultDataAccessException exception) {
+			
+		} catch (Exception e) {
+
+		}
 		// mandar verificao Url
 		// mandar url na tabela de verificao
 		// mandar email para o usuario com a url de verificação
@@ -57,7 +85,11 @@ public class UsuarioRepositoryImpl implements UsuarioRepository<Usuario> {
 
 	//@Query("SELECT COUNT(*) FROM Usuario u")
 	private Integer getEmailCount(String email) {
-		return jdbc.queryForObject(COUNT_USER_EMAIL_QUERY, Map.of("email", email), Integer.class);
+		return jdbc.queryForObject(UsuarioQuery.COUNT_USUARIO_EMAIL_QUERY, Map.of("email", email), Integer.class);
+	}
+
+	private SqlParameterSource getSqlParameterSource(Usuario usuario) {
+		return null;
 	}
 
 }
---
+++ src/main/java/com/luan/invoicetracker/query/UsuarioQuery.java
@@ -0,0 +1,8 @@
+package com.luan.invoicetracker.query;
+
+public class UsuarioQuery {
+	
+	public static final String COUNT_USUARIO_EMAIL_QUERY = "";
+	public static final String INSERT_USUARIO_QUERY = "";
+
+}
